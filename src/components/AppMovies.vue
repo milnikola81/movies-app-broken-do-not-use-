791fafd73c0867f364b9b8e9b1c830d6a943e4de
Nikola <milnikola81@gmail.com>
2018-08-21 18:28:50 +0200

    finished no.2

--- src/components/AppMovies.vue
+++ src/components/AppMovies.vue
@@ -1,11 +1,32 @@
 <template>
     <div>
-        <h1>App Movies</h1>
+        <div class="wrapper">
+        <h1 style="padding-top: 1rem; padding-bottom: 1rem"><strong>App Movies | Vue Project</strong></h1>
+        <div class="post" v-for="(movie, index) in movies" :key="index">
+            <h2 style="margin-bottom: 2rem">{{movie.title}}</h2>
+            <p style="color: #999999; margin-bottom: 2rem">Published on {{movie.releaseDate}}</p>
+            <p style="color: #999999; margin-bottom: 2rem">Genre {{movie.genre}}</p>
+            <p style="color: #999999; margin-bottom: 2rem">Director {{movie.director}}</p>
+        </div>
+    </div>
     </div>
 </template>
 
 <script>
+import { movies } from '../services/Movies'
+
 export default {
+    data () {
+        return {
+            movies: []
+        }
+    },
+    created () {
+        movies.getAll()
+        .then((response) => {
+            this.movies = response.data
+        })
+    }
     
 }
 </script>
---
+++ src/services/Movies.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+// importujem axios
+
+export default class Movies {
+    constructor () {
+      axios.defaults.baseURL = 'http://localhost:3000/api/'
+      // definisem default url na koji ce se nastavljati http requesti
+    }
+
+    // getAll() {
+    //     return axios.get('movies')
+    // }
+}
+
+export const movies = new Movies ()
