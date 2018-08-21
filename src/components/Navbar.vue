7649058ae5bc9e792225a3919598367c5a6bd1bf
Nikola <milnikola81@gmail.com>
2018-08-21 18:13:13 +0200

    finished no.1

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+            <a class="navbar-brand" href="#">Movies</a>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueRouter from 'vue-router'
+import AppMovies from './components/AppMovies.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {path: '/movies', component: AppMovies},
+  {path: '/', redirect: '/movies'}
+  // {path: '/contacts/:id', component: Contacts, name: 'contact-details'}
+];
+
+const router = new VueRouter({
+  routes
+});
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App), router
 }).$mount('#app')
