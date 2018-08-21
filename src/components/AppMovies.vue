22bb4835f00ead43ae7afcab224e9cf343beb3b3
Nikola <milnikola81@gmail.com>
2018-08-21 18:45:49 +0200

    finished no.4

--- src/components/AppMovies.vue
+++ src/components/AppMovies.vue
@@ -1,21 +1,28 @@
 <template>
     <div>
-        <div class="wrapper">
-        <h1 style="padding-top: 1rem; padding-bottom: 1rem"><strong>App Movies | Vue Project</strong></h1>
-        <div class="post" v-for="(movie, index) in movies" :key="index">
-            <h2 style="margin-bottom: 2rem">{{movie.title}}</h2>
-            <p style="color: #999999; margin-bottom: 2rem">Published on {{movie.releaseDate}}</p>
-            <p style="color: #999999; margin-bottom: 2rem">Genre {{movie.genre}}</p>
-            <p style="color: #999999; margin-bottom: 2rem">Director {{movie.director}}</p>
-        </div>
-    </div>
+        <!-- <div class="wrapper">
+            <h1 style="padding-top: 1rem; padding-bottom: 1rem"><strong>App Movies | Vue Project</strong></h1>
+            <div class="post" v-for="(movie, index) in movies" :key="index">
+                <h2 style="margin-bottom: 2rem">{{movie.title}}</h2>
+                <p style="color: #999999; margin-bottom: 2rem">Published on {{movie.releaseDate}}</p>
+                <p style="color: #999999; margin-bottom: 2rem">Genre {{movie.genre}}</p>
+                <p style="color: #999999; margin-bottom: 2rem">Director {{movie.director}}</p>
+            </div>
+        </div> -->
+        <ul>
+            <MovieRow v-for="(movie, index) in movies" :key="index" :movie = "movie" />
+        </ul>
     </div>
 </template>
 
 <script>
 import { movies } from '../services/Movies'
+import MovieRow from '../components/MovieRow.vue'
 
 export default {
+    components: {
+        MovieRow
+    },
     data () {
         return {
             movies: []
---
+++ src/components/MovieRow.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <li>{{movie.title}}, {{movie.director}}, {{movie.releaseDate}}, {{movie.genre}}</li>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['movie'] 
+}
+</script>
+
