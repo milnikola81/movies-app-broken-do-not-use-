18732674de71b27dbc3f57c620da6ea6afcee4d4
Nikola <milnikola81@gmail.com>
2018-08-31 20:35:33 +0200

    finished no.2.2

--- src/services/Movies.js
+++ src/services/Movies.js
@@ -3,13 +3,26 @@ import axios from 'axios'
 
 export default class Movies {
     constructor () {
-      axios.defaults.baseURL = 'http://localhost:3000/api/'
+      axios.defaults.baseURL = 'http://localhost:8000/api/'
       // definisem default url na koji ce se nastavljati http requesti
     }
 
     getAll() {
         return axios.get('movies')
     }
+
+    // add(movie) {
+    //     return axios.post('movies', movie)
+    // }
+    // getMovie(id) {
+    //     return axios.get(`movies/${id}`)
+    // }
+    // deleteMovie(movie) {
+    //     return axios.delete(`movies/${movie.id}`)
+    // }
+    // edit(movie) {
+    //     return axios.put(`movies/${movie.id}`, movie)
+    // }
 }
 
 export const movies = new Movies ()
